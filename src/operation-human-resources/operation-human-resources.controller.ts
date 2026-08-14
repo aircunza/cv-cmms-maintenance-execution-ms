d30f8c4606509e732ede9507e2d462469b7510a4
aircunza <135544542+aircunza@users.noreply.github.com>
2026-08-14 16:52:31 -0500

    style(controller): standardize quotation marks and formatting in operation human resources controller

--- src/operation-human-resources/operation-human-resources.controller.ts
+++ src/operation-human-resources/operation-human-resources.controller.ts
@@ -1,33 +1,49 @@
-import { Controller } from '@nestjs/common';
-import { MessagePattern, Payload } from '@nestjs/microservices';
-import { OperationHumanResourcesService } from './operation-human-resources.service';
+import { Controller } from "@nestjs/common";
+import { MessagePattern, Payload } from "@nestjs/microservices";
+import { OperationHumanResourcesService } from "./operation-human-resources.service";
 import {
   CreateOperationHrDto,
   UpdateOperationHrDto,
   CancelOperationHrDto,
   FindAllOperationHrDto,
-} from './dto';
+} from "./dto";
 
-@Controller('operation-human-resources')
+@Controller("operation-human-resources")
 export class OperationHumanResourcesController {
-  constructor(private readonly operationHumanResourcesService: OperationHumanResourcesService) {}
+  constructor(
+    private readonly operationHumanResourcesService: OperationHumanResourcesService,
+  ) {}
 
-  @MessagePattern('operation.hr.create')
-  create(@Payload() dto: CreateOperationHrDto & { actorId: string; actorName: string; operationCode: number }) {
+  @MessagePattern("operation.hr.create")
+  create(
+    @Payload()
+    dto: CreateOperationHrDto & {
+      actorId: string;
+      actorName: string;
+      operationCode: number;
+    },
+  ) {
     return this.operationHumanResourcesService.create(dto);
   }
 
-  @MessagePattern('operation.hr.find.all')
+  @MessagePattern("operation.hr.find.all")
   findAll(@Payload() dto: FindAllOperationHrDto = {}) {
     return this.operationHumanResourcesService.findAll(dto);
   }
 
-  @MessagePattern('operation.hr.update')
-  update(@Payload() dto: UpdateOperationHrDto & { id: number; actorId: string; actorName: string }) {
+  @MessagePattern("operation.hr.update")
+  update(
+    @Payload()
+    dto: UpdateOperationHrDto & {
+      id: number;
+      actorId: string;
+      actorName: string;
+    },
+  ) {
     return this.operationHumanResourcesService.update(dto);
   }
 
-  @MessagePattern('operation.hr.cancel')
+  @MessagePattern("operation.hr.cancel")
   cancel(
     @Payload()
     dto: CancelOperationHrDto & {
